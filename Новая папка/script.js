e17f6ffc6468e79269beab7f7df895b5aa2146d5
Andrei-007 <76580875+Andrei-007@users.noreply.github.com>
2021-01-13 22:43:25 +0600

    Add files via upload

---
+++ Новая папка/script.js
@@ -0,0 +1,9 @@
+var result, num1, num2, num3;
+
+num1 = prompt("Введите первую цифру:");
+num2 = prompt("Введите вторую цифру:");
+num3 = prompt("Введите третью цифру:");
+
+result=num1+num2+num3;
+
+alert("Ваша Сумма:"+result)
